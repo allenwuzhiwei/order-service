597617948205208b7b2aacb556b0a652dc51c0a5
allenwuzhiwei <allenzhiwei0@gmail.com>
2025-04-07 21:33:17 +0700

    first commit

---
+++ src/main/java/com/nusiss/orderservice/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package com.nusiss.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.nusiss.commonservice.feign", "com.nusiss.productservice.client"})
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
