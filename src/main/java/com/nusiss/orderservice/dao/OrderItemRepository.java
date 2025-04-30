01852cdd225790413f0347564cbb164bad04f559
SONG YINRUI <78004370+RickSong218@users.noreply.github.com>
2025-04-30 11:52:09 +0800

    Update the order item data access interface OrderItemRepository to support query by order ID

--- src/main/java/com/nusiss/orderservice/dao/OrderItemRepository.java
+++ src/main/java/com/nusiss/orderservice/dao/OrderItemRepository.java
@@ -1,4 +1,23 @@
 package com.nusiss.orderservice.dao;
 
 public interface OrderItemRepository {
+import com.nusiss.orderservice.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/*
+ OrderItemRepository - JPA 数据访问接口
+ 提供对 order_items 表的操作
+ */
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    /*
+     根据订单 ID 查询该订单的所有订单项
+     @param orderId 所属订单 ID
+     @return 商品项列表
+     */
+    List<OrderItem> findByOrderId(Long orderId);
 }
