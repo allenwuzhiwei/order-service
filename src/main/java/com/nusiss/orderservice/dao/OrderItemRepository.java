51dd1b8626c0faa7e56f3aebf6d77ed07278e81e
SONG YINRUI <78004370+RickSong218@users.noreply.github.com>
2025-04-30 11:53:14 +0800

    Update the order item data access interface OrderItemRepository to support query by order ID

--- src/main/java/com/nusiss/orderservice/dao/OrderItemRepository.java
+++ src/main/java/com/nusiss/orderservice/dao/OrderItemRepository.java
@@ -1,6 +1,5 @@
 package com.nusiss.orderservice.dao;
 
-public interface OrderItemRepository {
 import com.nusiss.orderservice.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
